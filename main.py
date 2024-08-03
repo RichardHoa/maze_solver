07d73fb9a4963f4d3b78f506304737ae5fd00ecc
Hoa (Richard) <hoa62414@gmail.com>
2024-08-03 15:51:31 +0700

    some random comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from Canvas import Window, Line, Point, Cell, Maze
-import time
+from Canvas import Window, Maze
 
 
 def main():
@@ -7,7 +6,7 @@ def main():
     screen_width = 1400
     screen_height = 700
     win = Window(screen_width, screen_height)
-    
+
     cell_size_x = 40
     cell_size_y = 40
     x = 10
@@ -25,14 +24,10 @@ def main():
     def continue_after_delay():
         maze.reset_cell_visited()
         maze.solve()
-    
+
     win.root.after(15000, continue_after_delay)
 
     win.wait_for_close()
-
-    
-
-
 
 
 main()
